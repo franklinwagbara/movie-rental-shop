d0038309a735b683480ff98ba5938f92c699e247
Franklin Wagbara <wagbarafranklin@yahoo.com>
2022-02-06 12:04:18 +0100

    updated NavBar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,40 @@ import React, { Component } from "react";
 class NavBar extends Component {
   state = {};
   render() {
-    return <div>Todo NavBar</div>;
+    return (
+      <nav className="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
