c0b725c39ec0423eb8e91191cfa9d70db474f3be
Franklin Wagbara <wagbarafranklin@yahoo.com>
2022-02-08 17:34:50 +0100

    modified footer main components and added intro component

---
+++ src/components/intro.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./css/intro.css";
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div>
+        <span>Intro</span>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./css/theme.css";
 import Banner from "./banner";
+import Intro from "./intro";
 
 class Main extends Component {
   state = {};
   render() {
     return (
-      <div className="row back-color">
+      <div className="row center back-color">
         <Banner />
+        <Intro />
+        <Intro />
       </div>
     );
   }
