f61674003d9a5578ff37ac9c5684981922cc3ca2
Franklin Wagbara <wagbarafranklin@yahoo.com>
2022-03-01 16:47:16 +0100

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,1 @@
-import React, { Component } from "react";
-import "./App.css";
-import Game from "./components/game";
-
-class App extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <Game />
-      </div>
-    );
-  }
-}
-
-export default App;
+im;
